02da3957bc71262552c3792f7991cf0ede6b1284
PramitaDash <dashpramita34@gmail.com>
2025-05-26 00:49:29 +0530

    third

---
+++ StreamApiPrep/src/greeksAndGreeksBasic/LargestElement.java
@@ -0,0 +1,16 @@
+package greeksAndGreeksBasic;
+
+public class LargestElement {
+
+    public static void main(String[] args) {
+        int a[]={1,4,2,6,7};
+        int max = a[0];
+
+        for(int i=0;i<a.length;i++){
+            if(a[i]>max){
+                max = a[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
